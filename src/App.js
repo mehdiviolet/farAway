95bdc9e97f1e7e202e69425dfa7ae05a74cb4cb5
mehdiviolet <mehdiviolet@yahoo.com>
2024-08-20 10:18:39 +0200

    modify  commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { type } from "@testing-library/user-event/dist/type";
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
-
 export default function App() {
   const [newItems, setnewItems] = useState([]);
 
   function handleNewItems(item) {
     setnewItems((items) => [...items, item]);
+  }
+
+  function deleteItem(id) {
+    // console.log(newItems.filter((item) => item.id !== id));
+    // const newI = { name: "Me" };
+    // console.log(setnewItems((item) => [...item, newI]));
+    // console.log(setnewItems((newItems) => [...newItems, newI]));
+    // console.log(setnewItems((items) => items.filter(items.id !== id)));
+
+    setnewItems((items) => items.filter((item) => item.id !== id));
   }
 
   function updateElement(id) {
@@ -29,6 +34,7 @@ export default function App() {
         newItems={newItems}
         setnewItems={setnewItems}
         updateElement={updateElement}
+        deleteItem={deleteItem}
       />
       <Stats newItems={newItems} />
     </div>
@@ -40,10 +46,8 @@ function Logo() {
 }
 
 function Form({ addHandleNewItems }) {
-  // const [item, setItem] = useState("");
   const [quantity, setQuantity] = useState(1);
   const [description, setDescription] = useState("");
-  // const [packed, setPacked] = useState(false);
 
   function handlerList(e) {
     e.preventDefault();
@@ -54,7 +58,9 @@ function Form({ addHandleNewItems }) {
       packed: false,
     };
     addHandleNewItems(newItem);
-    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+    // console.log(newItem);
   }
   return (
     <form className="add-form" onSubmit={handlerList}>
@@ -81,31 +87,56 @@ function Form({ addHandleNewItems }) {
   );
 }
 
-function PackingList({ newItems, setnewItems, updateElement }) {
+function PackingList({ newItems, updateElement, deleteItem, setnewItems }) {
+  // const sortItem = newItems.sort((b, a) => a.description - b);
+  // console.log(...sortItem);
+  // const [input, setInput] = useState(false);
+  // const [description, setDescription] = useState(false);
+  // const [packed, setPacked] = useState(false);
+  const [sortedBy, setSortedBy] = useState("input");
+
+  function handleChange(e) {
+    setSortedBy(e.target.value);
+    // if (value === "description") setDescription(!description);
+    // if (value === "input") setInput(!input);
+    // if (value === "packed") setPacked(!packed);
+  }
+  let sortedItems;
+  if (sortedBy === "input") sortedItems = newItems;
+  if (sortedBy === "description")
+    sortedItems = newItems
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortedBy === "packed")
+    sortedItems = newItems
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {newItems.map((item) => {
+        {sortedItems.map((item) => {
           return (
             <ListItem
               item={item}
               key={item.id}
-              newItems={newItems}
-              setnewItems={setnewItems}
               updateElement={updateElement}
+              deleteItem={deleteItem}
             />
           );
         })}
       </ul>
+      <div className="actions">
+        <select onChange={handleChange} value={sortedBy}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
-function ListItem({ item, newItems, setnewItems, updateElement }) {
-  function deletItem() {
-    console.log(newItems);
-    setnewItems((items) => items.filter((el) => el.id !== item.id));
-  }
-
+function ListItem({ item, updateElement, deleteItem }) {
   return (
     <>
       <li>
@@ -117,7 +148,7 @@ function ListItem({ item, newItems, setnewItems, updateElement }) {
         <span style={item.packed ? { textDecoration: "line-through" } : {}}>
           {item.quantity} {item.description}
         </span>
-        <button onClick={deletItem}>❌</button>
+        <button onClick={() => deleteItem(item.id)}>❌</button>
       </li>
     </>
   );
@@ -132,7 +163,7 @@ function Stats({ newItems }) {
     );
 
   const itemPacked = newItems.filter((item) => item.packed === true);
-  const percentage = (itemPacked.length / newItems.length) * 100;
+  const percentage = Math.round((itemPacked.length / newItems.length) * 100);
 
   return (
     <footer className="stats">
